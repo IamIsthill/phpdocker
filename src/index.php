ad2687ba6876f033bb356861fe9905428e2da6bd
IamIsthill <159409061+IamIsthill@users.noreply.github.com>
2024-03-21 23:26:18 +0800

    Test if it is routing to another page

--- src/index.php
+++ src/index.php
@@ -21,3 +21,18 @@ try {
     echo "Connection failed: " . $e->getMessage(); // If connection fails, this message will be displayed along with the error message
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <button><a href="./test.php">Go</a></button>
+  
+</body>
+</html>
+
+
